a6f95fb4139d21c646ac89064fd3a29de63ba1c0
Matt Simonis <matts12290@gmail.com>
2016-02-15 18:38:22 -0500

    Fixes #2
    Added logic for custom field name field id to fill in with no
    underscores.

--- app/js/apiname.js
+++ app/js/apiname.js
@@ -3,13 +3,19 @@ var BfsApiName = BfsApiName || {};
 BfsApiName.init = function() {
   console.log('BfsApiName.init');
 
-  if ($('input#MasterLabel').size() === 0) {
+  if ($('input#MasterLabel').size() === 0 ||
+      ($('input#DeveloperName').size() === 0 &&
+        $('input#Name').size() === 0)) {
     return;
   }
   
   $('input#MasterLabel').blur(function() {
     var label = $(this).val();
-    $('input#DeveloperName').val(label.replace(/ /g, ''));
+    if ($('input#DeveloperName').size() === 1) {
+      $('input#DeveloperName').val(label.replace(/ /g, ''));
+    } else if ($('input#Name').size() === 1) {
+      $('input#Name').val(label.replace(/ /g, ''));
+    }
   });
 };
 
