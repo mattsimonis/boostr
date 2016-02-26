56efff792ce0c02cb43c453ba81a02a1d4ccb4cb
Matt Simonis <matts12290@gmail.com>
2016-02-25 20:14:18 -0500

    Fix for oldVersion

--- app/js/background.js
+++ app/js/background.js
@@ -1,5 +1,6 @@
 var currentVersion = chrome.app.getDetails().version;
 var oldVersion = localStorage['oldVersion'];
+localStorage.setItem('oldVersion', currentVersion);
 
 if (oldVersion != currentVersion) {
   if (oldVersion == undefined) { // first run
