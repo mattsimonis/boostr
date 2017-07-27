171c57300ad9d2ae602b65e71fca3a45f402aa03
lfreeland <LukeJFreeland@gmail.com>
2017-07-27 17:23:32 -0400

    Added Lightning Experience support so that when the generic flow error appears, the "View Offending Process Builder or Flow" link is shown there too. Previously, it was only showing in Salesforce Classic. This was implemented for Boostr issue #42 at https://github.com/mattsimonis/boostr/issues/42 (#43)

--- app/js/flowerrorlink.js
+++ app/js/flowerrorlink.js
@@ -1,17 +1,65 @@
 var BfsFlowErrorLink = BfsFlowErrorLink || {};
+var genericFlowErrorMsgFragment = "A flow trigger failed to execute the flow with version ID";
 
-BfsFlowErrorLink.init = function() {
-  var genericFlowErrorTD = $('td:contains("A flow trigger failed to execute the flow with version ID")').filter(':last');
-  genericFlowErrorTD.each(function( index, element ) {
-    var versionIdRegEx = /(301\w{12})/g;
-    var elementJQ = $( element );
-    var elementText = elementJQ.text();
-    var matches = versionIdRegEx.exec(elementText);
-    var versionId = matches[0];
-	 
-    var offenderLink = '<a href="/designer/designer.apexp#Id=' + versionId + '">View Offending Process Builder or Flow</a>';
-    elementJQ.append(offenderLink);
+BfsFlowErrorLink.appendOffenderLink = function (errorContainerElement) {
+  var versionIdRegEx = /(301\w{12})/g;
+  var elementJQ = $(errorContainerElement);
+  var elementText = elementJQ.text();
+  var matches = versionIdRegEx.exec(elementText);
+  var versionId = matches[0];
+
+  var offenderLink = '<a href="/designer/designer.apexp#Id=' + versionId + '">View Offending Process Builder or Flow</a>';
+  elementJQ.append(offenderLink);
+}
+
+BfsFlowErrorLink.findAndAppendOffenderLinkInClassic = function () {
+  var genericFlowErrorTD = $('td:contains(' + genericFlowErrorMsgFragment + ')').filter(':last');
+  genericFlowErrorTD.each(function (index, element) {
+    BfsFlowErrorLink.appendOffenderLink(element);
   });
+}
+
+BfsFlowErrorLink.hasFlowError = function (mutations) {
+  // Since this code executes on each DOM addition or removal to the document,
+  // this low-level code was added to be as performant as possible.
+  for (var mutationIndex = 0; mutationIndex < mutations.length; ++mutationIndex) {
+    var mutation = mutations[mutationIndex];
+    if (mutation.addedNodes) {
+      for (var addedNodeIndex = 0; addedNodeIndex < mutation.addedNodes.length; ++addedNodeIndex) {
+        var addedNode = mutation.addedNodes[addedNodeIndex];
+
+        if (addedNode.innerText &&
+            addedNode.innerText.indexOf(genericFlowErrorMsgFragment) != -1) {
+          BfsFlowErrorLink.appendOffenderLink(addedNode);
+          return;
+        }
+      }
+    }
+  }
+}
+
+BfsFlowErrorLink.init = function () {
+  BfsFlowErrorLink.findAndAppendOffenderLinkInClassic();
+
+  // Only add the mutation observer if we're in Lightning Experience to avoid
+  // unnecessary overhead since the generic flow error in Classic is detectable
+  // on page load whereas we have to detect DOM changes in Lightning to detect the
+  // error.
+  var someLightningElement = document.getElementById('auraAppcacheProgress');
+
+  if (someLightningElement) {
+      var container = document;
+      var observer = new MutationObserver(BfsFlowErrorLink.hasFlowError);
+
+      observer.observe(container, {
+          childList: true,
+          attributes: false,
+          characterData: false,
+          subtree: true,
+          attributeOldValue: false,
+          characterDataOldValue: false
+      });
+  }
 }
 
 BfsFlowErrorLink.init();
