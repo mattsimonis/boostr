b615ce24fac767ae611480492be174a6f471eaf1
Matt Simonis <msimonis@nimbleuser.com>
2019-09-10 05:44:37 -0700

    Update for lightning experience search

--- app/js/setupsearch.js
+++ app/js/setupsearch.js
@@ -2,7 +2,41 @@ var BfsSetupSearch = BfsSetupSearch || {};
 
 BfsSetupSearch.init = function() {
   $(function() {
-    $('div.quickfindContainer input#setupSearch').val(' ');
+    if ($('div.quickfindContainer input#setupSearch').size() > 0) {
+      $('div.quickfindContainer input#setupSearch').val(' ');
+    } else {
+      BfsSetupSearch.lightningInit();
+    }
+  });
+}
+
+BfsSetupSearch.lightningInit = function() {
+  var targetNode = document.documentElement;
+  var config = { childList: true, subtree: true };
+
+  var observer = new MutationObserver(function(mutationsList, observer) {
+    for (var mutation of mutationsList) {
+      mutation.addedNodes.forEach(function(node) {
+        if (node.matches && node.matches('.tree-filter')) {
+          let input = document.querySelector('input.filter-box');
+          BfsSetupSearch.handleLightningInput(input);
+
+          observer.disconnect();
+        }
+      });
+    }
+  });
+
+  observer.observe(targetNode, config);
+}
+
+BfsSetupSearch.handleLightningInput = function(input) {
+  input.addEventListener('blur', event => {
+    if (event.relatedTarget.matches('body.desktop')) {
+      setTimeout(() => {
+        input.focus();
+      });
+    }
   });
 }
 
@@ -10,8 +44,6 @@ chrome.storage.sync.get({
   'setupsearch': true
 }, function(item) {
   if (item.setupsearch === true) {
-    if ($('div.quickfindContainer input#setupSearch').size() > 0) {
-      BfsSetupSearch.init();
-    }
+    BfsSetupSearch.init();
   }
 });
